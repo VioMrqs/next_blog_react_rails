53eecc3873dd49d800a0ebc80a966b5f6b91d65c
VioMrqs <violette.marquis@gmail.com>
2022-03-06 17:49:21 +0100

    made elmt post to display comment by post

---
+++ blog-front/src/components/Post.jsx
@@ -0,0 +1,68 @@
+import { useUserContext } from "../UserContext";
+import Cookies from "js-cookie";
+import { useState } from "react";
+import CommentsList from "./CommentsList";
+import Button from "./Button";
+import PostUpdateForm from "./PostUpdateForm";
+
+const Post = ({ post }) => {
+  const { user } = useUserContext();
+  const userToken = Cookies.get("token");
+  const [showUpdate, setShowUpdate] = useState(false);
+  const [showComments, setShowComments] = useState(false);
+
+  const deletePost = async (id) => {
+    const response = await fetch(`http://localhost:3000/posts/${id}`, {
+      headers: {
+        Authorization: `${userToken}`,
+        "Content-Type": "application/json",
+      },
+      method: "DELETE",
+    });
+    const result = await response.json();
+    console.log(result);
+  };
+
+  const handleDeleteSubmit = (id) => {
+    deletePost(id);
+    window.location.reload();
+  };
+
+  const handleUpdateSubmit = () => {
+    setShowUpdate(!showUpdate);
+  };
+
+  const handleShowComments = () => {
+    setShowComments(!showComments);
+  };
+
+  return (
+    <div key={post.id}>
+      <h2>{post.title}</h2>
+      <img
+        className="post__picture"
+        src={post.image_url}
+        alt="instagramable"
+        onClick={() => handleShowComments()}
+      ></img>
+      <div className="post__text__picture">{post.content}</div>
+      {showComments ? <CommentsList post={post} /> : null}
+      {user && user.id === post.user_id && (
+        <>
+          <Button
+            text={"Supprimer"}
+            onClick={() => handleDeleteSubmit(post.id)}
+            className={"button__small"}
+          />
+          <Button
+            text={"Modifier"}
+            onClick={() => handleUpdateSubmit()}
+            className={"button__small"}
+          />
+          {showUpdate ? <PostUpdateForm id={post.id} /> : null}
+        </>
+      )}
+    </div>
+  );
+};
+export default Post;
--- blog-front/src/components/PostsList.jsx
+++ blog-front/src/components/PostsList.jsx
@@ -1,45 +1,15 @@
-import Button from "./Button";
 import { useUserContext } from "../UserContext";
-import Cookies from "js-cookie";
-import PostUpdateForm from "./PostUpdateForm";
 import { useState } from "react";
 import Masonry from "react-masonry-css";
-import CommentsList from "./CommentsList";
+import Button from "./Button";
+import Post from "./Post";
 
 const PostsList = ({ data }) => {
   const { user } = useUserContext();
-  const userToken = Cookies.get("token");
-  const [showUpdate, setShowUpdate] = useState(false);
   const [filter, setFilter] = useState(false);
-  const [showComments, setShowComments] = useState(false);
-
-  const deletePost = async (id) => {
-    const response = await fetch(`http://localhost:3000/posts/${id}`, {
-      headers: {
-        Authorization: `${userToken}`,
-        "Content-Type": "application/json",
-      },
-      method: "DELETE",
-    });
-    const result = await response.json();
-    console.log(result);
-  };
-
-  const handleDeleteSubmit = (id) => {
-    deletePost(id);
-    window.location.reload();
-  };
-
-  const handleUpdateSubmit = () => {
-    setShowUpdate(!showUpdate);
-  };
 
   const handleFilterSubmit = () => {
     setFilter(!filter);
-  };
-
-  const handleShowComments = () => {
-    setShowComments(!showComments);
   };
 
   const setData = (data) => {
@@ -63,36 +33,9 @@ const PostsList = ({ data }) => {
         className="my-masonry-grid"
         columnClassName="my-masonry-grid_column"
       >
-        {setData(data).map((post) => {
-          return (
-            <div key={post.id}>
-              <h2>{post.title}</h2>
-              <img
-                className="post__picture"
-                src={post.image_url}
-                alt="instagramable"
-                onClick={() => handleShowComments()}
-              ></img>
-              <div className="post__text__picture">{post.content}</div>
-              {showComments ? <CommentsList post={post} /> : null}
-              {user && user.id === post.user_id && (
-                <>
-                  <Button
-                    text={"Supprimer"}
-                    onClick={() => handleDeleteSubmit(post.id)}
-                    className={"button__small"}
-                  />
-                  <Button
-                    text={"Modifier"}
-                    onClick={() => handleUpdateSubmit()}
-                    className={"button__small"}
-                  />
-                  {showUpdate ? <PostUpdateForm id={post.id} /> : null}
-                </>
-              )}
-            </div>
-          );
-        })}
+        {setData(data).map((post) => (
+          <Post post={post} />
+        ))}
       </Masonry>
       ;
     </div>
